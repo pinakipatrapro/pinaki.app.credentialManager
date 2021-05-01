d7a964902d54d68eb92ccc89bc5917b59ff8b68c
Pinaki Patra <Pinaki.Patra.pro@gmail.com>
2021-05-01 17:58:48 +0530

    Init

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["pinaki/app/credentialManager/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("pinaki.app.credentialManager.controller.MainView", {});
+});
