43bd786c5a1bdba9090c3d2753bc3085f023cae0
Pinaki Patra <Pinaki.Patra.pro@gmail.com>
2021-05-02 20:51:45 +0530

    Enhancements

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "pinaki/app/credentialManager/controller/BaseController",
     "sap/ui/model/Filter"
-], function (Controller,Filter) {
+], function (Controller, Filter) {
     "use strict";
 
     return Controller.extend("pinaki.app.credentialManager.controller.MainView", {
@@ -9,14 +9,14 @@ sap.ui.define([
             let aCreds = this.getView().getModel().getProperty('/aCreds');
             let template = JSON.parse(JSON.stringify(this.getView().getModel().getProperty('/crendentialTemplate')));
             template.lastChanged = new Date();
-            aCreds = [...aCreds,template];
-            this.getView().getModel().setProperty('/aCreds',aCreds);
+            aCreds = [...aCreds, template];
+            this.getView().getModel().setProperty('/aCreds', aCreds);
             this.getView().getModel().updateBindings();
             this.getView().getModel().firePropertyChange();
         },
-        toggleEditMode : function(oEvent){
+        toggleEditMode: function (oEvent) {
             var expectedState = !oEvent.getSource().getBindingContext().getObject().editMode;
-            this.getView().getModel().getProperty('/aCreds').forEach((e)=>{
+            this.getView().getModel().getProperty('/aCreds').forEach((e) => {
                 e.editMode = false;
             });
             oEvent.getSource().getBindingContext().getObject().editMode = expectedState;
@@ -25,56 +25,67 @@ sap.ui.define([
             oEvent.getSource().getModel().firePropertyChange();
             oEvent.getSource().getModel().refresh();
         },
-        onDeleteCredPress : function(oEvent){
+        onDeleteCredPress: function (oEvent) {
             var path = oEvent.getSource().getBindingContext().getPath();
-            var index =  path.split('/').pop();
+            var index = path.split('/').pop();
             let aCreds = this.getView().getModel().getProperty('/aCreds');
-            aCreds.splice(index,1);
-            this.getView().getModel().setProperty('/aCreds',aCreds);
+            aCreds.splice(index, 1);
+            this.getView().getModel().setProperty('/aCreds', aCreds);
             oEvent.getSource().getModel().updateBindings();
             oEvent.getSource().getModel().firePropertyChange();
             sap.m.MessageToast.show("Credentials Deleted Successfully", {
-                duration: 10000, 
+                duration: 10000,
             });
-            
+
         },
-        onCredSearch : function(oEvent){
-            var list  = this.getView().byId('credList');
+        onCredSearch: function (oEvent) {
+            var list = this.getView().byId('credList');
             list.getBinding('items').filter(new Filter({
                 filters: [
                     new Filter({
-                      path: 'name',
-                      operator: "Contains",
-                      value1: oEvent.getParameter('newValue')
+                        path: 'name',
+                        operator: "Contains",
+                        value1: oEvent.getParameter('newValue')
                     }),
                     new Filter({
-                      path: 'username',
-                      operator: "Contains",
-                      value1: oEvent.getParameter('newValue')
+                        path: 'username',
+                        operator: "Contains",
+                        value1: oEvent.getParameter('newValue')
                     })
-                  ],
-                  and: false
+                ],
+                and: false
             }))
         },
-        onAfterRendering : function(){
+        onAfterRendering: function () {
             this.loadFromLocal();
             sap.m.MessageToast.show("Tap on an item to view, edit", {
-                duration: 10000, 
+                duration: 10000,
             });
         },
-        changeTheme : function(oEvent){
-            sap.ui.getCore().getConfiguration().getTheme() == "sap_fiori_3_dark"? 
-            sap.ui.getCore().applyTheme('sap_fiori_3') : sap.ui.getCore().applyTheme('sap_fiori_3_dark');
+        changeTheme: function (oEvent) {
+            sap.ui.getCore().getConfiguration().getTheme() == "sap_fiori_3_dark" ?
+                sap.ui.getCore().applyTheme('sap_fiori_3') : sap.ui.getCore().applyTheme('sap_fiori_3_dark');
         },
-        reload : function(){
+        reload: function () {
             location.reload(true);
         },
-        downloadCSV : function(){
-            downloadCSVFromJson(`CredentialManager.csv`,this.getView().getModel().getProperty('/aCreds'))
+        downloadCSV: function () {
+            downloadCSVFromJson(`CredentialManager.csv`, this.getView().getModel().getProperty('/aCreds'))
         },
-        resetAllData : function(){
-            this.getView().getModel().setProperty('/aCreds',[]);
+        resetAllData: function () {
+            this.getView().getModel().setProperty('/aCreds', []);
             this.getView().getModel().firePropertyChange();
+        },
+        openAboutDialog: function () {
+            if (!this._oDialog) {
+                this._oDialog = sap.ui.xmlfragment("pinaki.app.credentialManager.fragment.Info",this);
+                this.getView().addDependent(this._oDialog);
+            }
+
+            this._oDialog.open();
+        },
+        closeDialog : function(oEvent){
+            oEvent.getSource().getParent().close();
         }
     });
 });
